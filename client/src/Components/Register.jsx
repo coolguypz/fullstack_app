752e07ccf9cbcc6b019ac29bf1d345e9130e699d
peter zhang <peterzhang@peters-iMac.attlocal.net>
2022-02-25 21:55:26 -0800

    update format

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -1,19 +1,17 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './register.css';
 import Form from './Form';
 import axios from 'axios';
 import Display from './Display';
 
 const Register = (props) => {
-
-    const [apiData, setApiData] = useState();
-    const [location, setlocation] = useState();
-    const [inputData, setinputData] = useState({
-    username: '',
-    password: '',
-    email: '',
-  });
-
+	const [apiData, setApiData] = useState();
+	const [location, setlocation] = useState();
+	const [inputData, setinputData] = useState({
+		username: '',
+		password: '',
+		email: '',
+	});
 
 	const getLocation = async () => {
 		await axios
@@ -21,76 +19,74 @@ const Register = (props) => {
 			.then((res) => setlocation(res.data));
 	};
 
-  const getAPIData = async () => {
+	const getAPIData = async () => {
 		await axios
 			.get(`http://localhost:3001/api`)
 			.then((res) => setApiData(res.data));
 	};
 
-useEffect(() => {
+	useEffect(() => {
 		getAPIData();
-    getLocation();
+		getLocation();
 	}, []);
-   
 
-  const handleChange = (e) => {
-    setinputData({ ...inputData, [e.target.name]: e.target.value });
-  };
+	const handleChange = (e) => {
+		setinputData({ ...inputData, [e.target.name]: e.target.value });
+	};
 
-  const handleSubmit = () => {
-    axios.post(`http://localhost:3001/api/post`, {
-      ...inputData,
-      ip: location.IPv4,
-      country: location.country_name,
-      state: location.state,
-      city: location.city,
-      zipcode: location.postal
-    })
-      setApiData([
-        ...apiData,
-        {
-        ...inputData,
-        ip: location.IPv4,
-        country: location.country_name,
-        state: location.state,
-        city: location.city,
-        zipcode: location.postal
-        }]
-        )
-  };
+	const handleSubmit = () => {
+		axios.post(`http://localhost:3001/api/post`, {
+			...inputData,
+			ip: location.IPv4,
+			country: location.country_name,
+			state: location.state,
+			city: location.city,
+			zipcode: location.postal,
+		});
+		setApiData([
+			...apiData,
+			{
+				...inputData,
+				ip: location.IPv4,
+				country: location.country_name,
+				state: location.state,
+				city: location.city,
+				zipcode: location.postal,
+			},
+		]);
+	};
 
-  const [update, setupdate] = useState()
+	const [update, setupdate] = useState();
 
-    const handleUpdate = (id) => {
-      apiData.forEach(v => {
-        if (v.id  === id){
-          console.log(v.email,v.id,v.id)
-        }
-      })
-      // axios.put(`http://localhost:3001/api/update/`,{
+	const handleUpdate = (id) => {
+		apiData.forEach((v) => {
+			if (v.id === id) {
+				console.log(v.email, v.id, v.id);
+			}
+		});
+		// axios.put(`http://localhost:3001/api/update/`,{
 
-      // })
+		// })
+	};
 
-    }
+	const handleDelete = (id) => {
+		axios.delete(`http://localhost:3001/api/delete/${id}`);
+	};
 
-
-  const handleDelete = (id) => {
-    axios.delete(`http://localhost:3001/api/delete/${id}`)
-  }
-
-
-
-  return (
-    <div className='register'>
-      <Form
-        state={inputData}
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-      />
-      <Display data={apiData} handleDelete={handleDelete} handleUpdate={handleUpdate}/>
-      
-    </div>
-  );
+	return (
+		<div className='register'>
+			<Form
+				state={inputData}
+				handleSubmit={handleSubmit}
+				handleChange={handleChange}
+			/>
+			<Display
+				data={apiData}
+				handleDelete={handleDelete}
+				handleUpdate={handleUpdate}
+			/>
+		</div>
+	);
 };
 
 export default Register;
--- server.js
+++ server.js
@@ -9,54 +9,53 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api', async (req, res) => {
-  db.execute(`select * from test;`, (err, result) => {
-    if (err) {
-      res.send('ERROR : ', err);
-    } else {
-      res.send(result);
-    }
-  });
+	db.execute(`select * from test;`, (err, result) => {
+		if (err) {
+			res.send('ERROR : ', err);
+		} else {
+			res.send(result);
+		}
+	});
 });
 
 app.post('/api/post', async (req, res) => {
-  console.log('req.body: ', req.body);
-  const { username, password, email, ip, country, state, city, zipcode } =
-    req.body;
-  await db.execute(
-    `insert into test 
+	console.log('req.body: ', req.body);
+	const { username, password, email, ip, country, state, city, zipcode } =
+		req.body;
+	await db.execute(
+		`insert into test 
     (username,password,email,ip,country,state,city,zipcode)
     values
     ("${username}","${password}","${email}","${ip}","${country}","${state}","${city}","${zipcode}"); `,
-    (err, result) => {
-      if (err) {
-        res.send('ERROR : ', err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
+		(err, result) => {
+			if (err) {
+				res.send('ERROR : ', err);
+			} else {
+				res.send(result);
+			}
+		},
+	);
 });
 
-app.delete(`/api/delete/:id`, (req,res)=>{
-  console.log("first: ",req.params)
-  const id = req.params.id;
-  const del = `delete from test where id = ${id}`
-  db.execute(del,(err,result)=>{
-    if(err) console.log(err)
-    else console.log("delete sucess")
-  })
-})
+app.delete(`/api/delete/:id`, (req, res) => {
+	console.log('first: ', req.params);
+	const id = req.params.id;
+	const del = `delete from test where id = ${id}`;
+	db.execute(del, (err, result) => {
+		if (err) console.log(err);
+		else console.log('delete sucess');
+	});
+});
 
-app.put(`/api/update`, (req,res)=>{
-  
-  const id = req.body.id;
-  const email = req.body.email;
+app.put(`/api/update`, (req, res) => {
+	const id = req.body.id;
+	const email = req.body.email;
 
-  const upd = `update test set email = ${email} where id = ${id}`
-  db.execute(upd,(err,result)=>{
-    if(err) console.log(err)
-    else console.log("delete sucess")
-  })
-})
+	const upd = `update test set email = ${email} where id = ${id}`;
+	db.execute(upd, (err, result) => {
+		if (err) console.log(err);
+		else console.log('delete sucess');
+	});
+});
 
 app.listen(PORT, () => console.log('server is listening on PORT ' + PORT));
